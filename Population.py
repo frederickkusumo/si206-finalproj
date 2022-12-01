2362c3ce34443722ca45da808b0d2a4d44cd71fe
avaas19 <avaas@umich.edu>
2022-12-01 10:01:08 -0500

    Update API
    Update API

--- Population.py
+++ Population.py
@@ -44,7 +44,7 @@ def main():
     cityData_list = [("New York","New York","36","51000") , ("Los Angeles","California", "06", "44000") , ("Seattle","Washington","53","63000") , ("Chicago","Illinois","17","14000") , ("Houston","Texas","48","35000") , ("Dallas","Texas","48","19000") , ("Austin","Texas","48","05000") , ("San Francisco","California","06","67000") , ("Denver","Colorado","08","20000"), ("Boston","Massachusetts","25","07000") , ("Cincinnati","Ohio","39","15000") , ("Miami","Florida","12","45000") , ("San Diego","California","06","66000") , ("Tucson","Arizona","04","77000") , ("Salt Lake City","Utah","49","67000"), ("Urban Honolulu", "Hawaii","15","71550") , ("Portland", "Oregon","41","59000") , ("Detroit","Michigan","26","22000") , ("Sacramento","California","06","64000") , ("San Jose","California","06","68000") , ("New Orleans","Louisiana","22","55000") , ("Atlanta","Georgia","13","04000") , ("Minneapolis","Minnesota","27","43000") , ("Orlando","Florida","12","53000") , ("Phoenix","Arizona","04","55000")]
 
     for num in range(0,len(cityData_list)):
-        #API_URL = get_request_url(cityData_list[num][3], cityData_list[num][2])
+        API_URL = get_request_url(cityData_list[num][3], cityData_list[num][2])
         get_data_using_cache(cityData_list[num][0],cityData_list[num][3], cityData_list[num][2],city_cache_file)
 
 
